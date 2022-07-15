0b990856ab6146abe7c23bfd5708eab18642afce
Quang Ngoc <ngocquang2509@gmail.com>
2022-07-15 18:16:44 +0700

    Inline if with && operator

--- src/index.js
+++ src/index.js
@@ -236,5 +236,19 @@ class LoginControl extends React.Component {
   }
 }
 
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 && (
+        <h2>You have {unreadMessages.length} unread messages.</h2>
+      )}
+    </div>
+  );
+}
+
+const messages = ["React", "Re: React", "Re:Re: React"];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<LoginControl />);
+root.render(<Mailbox unreadMessages={messages} />);
