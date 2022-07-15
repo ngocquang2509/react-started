5443b89a80a348894d735b63042607572cd00e7e
Quang Ngoc <ngocquang2509@gmail.com>
2022-07-15 18:19:21 +0700

    Prevent rendering

--- src/index.js
+++ src/index.js
@@ -250,5 +250,38 @@ function Mailbox(props) {
 
 const messages = ["React", "Re: React", "Re:Re: React"];
 
+//Prevent render
+function WariningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+  return <div>Warning!</div>;
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState((prevState) => ({
+      showWarning: !prevState.showWarning,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WariningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Mailbox unreadMessages={messages} />);
+root.render(<Page />);
