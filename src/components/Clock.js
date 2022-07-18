3a9f9d2d9e670e6ada1e61fb1da3e0fe22206475
Quang Ngoc <ngocquang2509@gmail.com>
2022-07-18 10:53:18 +0700

    change folder structure

---
+++ src/components/Clock.js
@@ -0,0 +1,41 @@
+import React from "react";
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Now is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
+export default Clock;
