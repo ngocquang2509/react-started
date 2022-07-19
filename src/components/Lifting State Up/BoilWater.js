8fa1e85e1fbb11ec76bd22fc32aef08d968a1f66
Quang Ngoc <ngocquang2509@gmail.com>
2022-07-19 15:36:57 +0700

    lifting state up updated

--- src/components/Lifting State Up/BoilWater.js
+++ src/components/Lifting State Up/BoilWater.js
@@ -8,27 +8,61 @@ function Boiling(props) {
   return <p>The water would not boil</p>;
 }
 
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function convertTemp(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 class Calulator extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      temperature: "",
-    };
+    this.handleChangeC = this.handleChangeC.bind(this);
+    this.handleChangeF = this.handleChangeF.bind(this);
+    this.state = { temperature: "", scale: "c" };
   }
 
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
+  handleChangeC(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+
+  handleChangeF(temperature) {
+    this.setState({ scale: "f", temperature });
   }
 
   render() {
+    const scale = this.state.scale;
     const temperature = this.state.temperature;
+    const celsius =
+      scale === "f" ? convertTemp(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? convertTemp(temperature, toFahrenheit) : temperature;
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <Boiling celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TempInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleChangeC}
+        />
+        <TempInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleChangeF}
+        />
+        <Boiling celsius={parseFloat(celsius)} />
+      </div>
     );
   }
 }
---
+++ src/components/Lifting State Up/TempInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+class TempInput extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.state = { temperature: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    //this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    //const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TempInput;
