71a70ab708869dfd8a1e814d040426c31032edf2
ngocquang2509 <ngocquang2509@gmail.com>
2022-08-04 17:30:48 +0700

    understand useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { useContext } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import Clock from "./components/Clock";
-import Content from "./components/useContext/Content";
-import {ThemeContext} from './components/useContext/ThemeContext'
-import {ThemeProvider} from './components/useContext/ThemeContext'
+// import Content from "./components/useContext/Content";
+// import {ThemeContext} from './components/useContext/ThemeContext'
+// import {ThemeProvider} from './components/useContext/ThemeContext'
+
+// Init state
+const initState = 0;
+
+// Define actions
+const INCRE_ACTION = "up";
+const DECRE_ACTION = "down";
+
+// Reducer
+const reducer = (state, action) => {
+  switch (action) {
+    case INCRE_ACTION:
+      return state + 1;
+    case DECRE_ACTION:
+      return state - 1;
+    default:
+      throw new Error("Invalid action");
+  }
+};
 
 function App() {
-  const context = useContext(ThemeContext);
+  //const context = useContext(ThemeContext);
+  const [count, dispatch] = useReducer(reducer, initState);
   return (
     <>
-      <ThemeProvider>
-        <Clock />
+      {/* <ThemeProvider>
+        
         <button onClick={context.toggleTheme}>Toggle Theme</button>
         <Content />
-      </ThemeProvider>
+      </ThemeProvider> */}
+      <Clock />
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(DECRE_ACTION)}>Down</button>
+      <button onClick={() => dispatch(INCRE_ACTION)}>Up</button>
     </>
   );
 }
