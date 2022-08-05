0aa1b1bea9a4d735429e0d6e3658e9bd262d20ea
ngocquang2509 <ngocquang2509@gmail.com>
2022-08-05 10:25:49 +0700

    remove all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import Game from "./components/TicTacToe/Game";
 import Clock from "./components/Clock";
-import NumberList from "./components/List";
-import NameForm from "./components/Forms/Forms";
-import Calulator from "./components/Lifting State Up/BoilWater";
-import Welcome from "./components/Composition/Welcome";
-import IncreDecre from "./components/IncreDecre";
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5];
   return (
     <div>
-      <Welcome />
       <Clock />
-      <h2>React started</h2>
-      <h3>Tic-tac-toe Game</h3>
-      <Game />
-      <h3>List in React</h3>
-      <NumberList numbers={numbers} />
-      <h3>Form in React</h3>
-      <NameForm />
-      <Calulator />
-      <IncreDecre />
     </div>
   );
 }
